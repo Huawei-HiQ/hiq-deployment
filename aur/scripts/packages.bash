4c822b304a07acf0534a966e1d1ac6ed2b96c1ea
Damien Nguyen <damien1@huawei.com>
2020-10-07 18:38:42 +0200

    Merge branch feature/aur into master
    Squashed commit of the following:
    
    commit 11d26cc75b0aabf9fea66fbdd8387ae22d73afc4
    Author: Damien Nguyen <damien1@huawei.com>
    Date:   Wed Oct 7 18:34:13 2020 +0200
    
        Linux AUR: add jupyter-react 0.1.7

--- aur/scripts/packages.bash
+++ aur/scripts/packages.bash
@@ -1,4 +1,4 @@
 # Packages that need to be built first and then installed in order to build the
 # others
 build_and_install=()
-build=(openfermion openfermionprojectq)
+build=(openfermion openfermionprojectq jupyter-react)
