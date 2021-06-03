9f4501fe8d40a2d05140a23298c99d53ca1366d2
Damien Nguyen <damien1@huawei.com>
2021-06-03 19:53:25 +0200

    Linux DEB: add CMake 3.20.3

--- deb/deps/_cmake_config.bash
+++ deb/deps/_cmake_config.bash
@@ -1,11 +1,11 @@
-ppa_rev=3
+ppa_rev=6
 
 # ==============================================================================
 
 function pkg_download()
 {
     local pkg_name=$1
-    
+
     orig_tar_gz=$root/${pkg_name}_${pkg_ver}.orig.tar.gz
     if [ ! -f $orig_tar_gz ]; then
 	wget http://archive.ubuntu.com/ubuntu/pool/main/c/${pkg_name}/${pkg_name}_${pkg_ver}.orig.tar.gz 1>&2
@@ -18,7 +18,7 @@ function pkg_download()
 function pkg_prepare()
 {
     local pkg_dir=$1
-    backup_files $pkg_dir control rules
+    backup_files $pkg_dir control rules tests/testsuite
     if [ "$ubuntu_distro_num" == "16.04" ]; then
 	cat << EOF > $pkg_dir/debian/compat
 10
@@ -26,12 +26,25 @@ EOF
 	sed -i -e 's/debhelper-compat (= 12)/debhelper/' \
 	    -e '/python3-sphinx,/a \\t       python3-snowballstemmer,' \
 	    $pkg_dir/debian/control
+
+	sed -i -e "s/CTestTestUpload/'(CTestTestUpload|BootstrapTest|CTestCoverageCollectGCOV|RunCMake.File_Archive|RunCMake.CommandLineTar)'/" \
+	    $pkg_dir/debian/rules
+
+	sed -i -e 's/(CTestTestUpload|BootstrapTest)/(CTestTestUpload|BootstrapTest|CTestCoverageCollectGCOV|RunCMake.File_Archive|RunCMake.CommandLineTar)/' \
+	    $pkg_dir/debian/tests/testsuite
+
     elif [ "$ubuntu_distro_num" == "18.04" ]; then
 	cat << EOF > $pkg_dir/debian/compat
 12
 EOF
 	sed -i -e 's/debhelper-compat (= 12)/debhelper/' \
 	    $pkg_dir/debian/control
+
+	sed -i -e "s/CTestTestUpload/'(CTestTestUpload|BootstrapTest|CTestCoverageCollectGCOV|RunCMake.File_Archive|RunCMake.CommandLineTar)'/" \
+	    $pkg_dir/debian/rules
+
+	sed -i -e 's/(CTestTestUpload|BootstrapTest)/(CTestTestUpload|BootstrapTest|CTestCoverageCollectGCOV|RunCMake.File_Archive|RunCMake.CommandLineTar)/' \
+	    $pkg_dir/debian/tests/testsuite
     else
 	rm -f $pkg_dir/debian/compat
     fi
